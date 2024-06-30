8ad03974134e2b422bcf8435562559ed9e67c46f
Edbert Widjaja <157877834+edbertw@users.noreply.github.com>
2024-06-30 16:49:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 import streamlit as st
 
+# TEST OPEN AI API KEY
 openai.api_key = 'sk-proj-CpNemz0YDojvoNiBVMkAT3BlbkFJTwGgK0sv1UmubB4wRqw8'
 
 def get_transcript(url):
